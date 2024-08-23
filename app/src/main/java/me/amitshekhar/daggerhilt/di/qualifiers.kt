02f0f69b24555ff8df9635a5145b3f48aa05f0ab
BHAVEN SHAH <bhaven103shah@gmail.com>
2024-08-24 01:55:09 +0530

    Initial commit

---
+++ app/src/main/java/me/amitshekhar/daggerhilt/di/qualifiers.kt
@@ -0,0 +1,7 @@
+package me.amitshekhar.daggerhilt.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
---
+++ app/src/main/java/me/amitshekhar/daggerhilt/ui/topheadline/TopHeadlineViewModel.kt
@@ -0,0 +1,39 @@
+package me.amitshekhar.daggerhilt.ui.topheadline
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import me.amitshekhar.daggerhilt.data.model.Article
+import me.amitshekhar.daggerhilt.data.repository.TopHeadlineRepository
+import me.amitshekhar.daggerhilt.ui.base.UiState
+import me.amitshekhar.daggerhilt.utils.AppConstant.COUNTRY
+import javax.inject.Inject
+
+@HiltViewModel
+class TopHeadlineViewModel @Inject constructor(private val topHeadlineRepository: TopHeadlineRepository) :
+    ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<List<Article>>>(UiState.Loading)
+
+    val uiState: StateFlow<UiState<List<Article>>> = _uiState
+
+    init {
+        fetchNews()
+    }
+
+    private fun fetchNews() {
+        viewModelScope.launch {
+            topHeadlineRepository.getTopHeadlines(COUNTRY)
+                .catch { e ->
+                    _uiState.value = UiState.Error(e.toString())
+                }.collect {
+                    _uiState.value = UiState.Success(it)
+                }
+        }
+    }
+
+}
